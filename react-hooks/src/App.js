852de4da70271f1d09a9833acf0b0c5d5f09cbac
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-10 22:33:49 +0300

    Start react app / add components

---
+++ react-hooks/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
+// import HooksCounter from "./components/HooksCounter";
+import UseStateWithObjects from "./Components/UseStateWithObjects";
+
+function App() {
+  return <div className="App">
+    {/* <StatefulGreetingWithPrevState />
+      <HooksCounter /> */}
+    <UseStateWithObjects />
+  </div>;
+}
+
+export default App;
